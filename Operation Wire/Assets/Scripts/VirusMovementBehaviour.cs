7868956e6972f857b6b12b468c1099b6756316b9
Iain Cattermole <iain_c@live.co.uk>
2018-01-26 20:15:18 +0000

    folders

---
+++ Operation Wire/Assets/Scripts/VirusMovementBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusMovementBehaviour : MonoBehaviour {
+
+	private int size;
+
+	private float xSpeed;
+	private float ySpeed;
+
+	void Start () {
+		xSpeed = 0;
+		ySpeed = 0;
+		size = 5;
+	}
+
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+
+		}
+	}
+
+	void changeSize(int n) {
+		
+	}
+}
