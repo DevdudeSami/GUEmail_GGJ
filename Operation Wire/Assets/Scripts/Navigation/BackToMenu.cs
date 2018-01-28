10dca925c7738350ec8759d4037ea6a9fdbf3394
Iain Cattermole <iain_c@live.co.uk>
2018-01-28 15:47:05 +0000

    Links

---
+++ Operation Wire/Assets/Scripts/Navigation/BackToMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BackToMenu : MonoBehaviour{
+
+	public Button yourButton;
+
+	void Start() {
+		print("start");
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(goToScene);
+	}
+
+
+
+	public void goToScene() {
+		print("Loading");
+		SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+	}
+
+
+}
