ed15113c179dc10a823af20939565f6668d5c9f7
Iain Cattermole <iain_c@live.co.uk>
2018-01-28 15:23:07 +0000

    scene linking

---
+++ Operation Wire/Assets/Scripts/NetworkLinkBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkLinkBehaviour : MonoBehaviour {
+
+	public string linkedScene;
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		VirusMovementBehaviour virus = coll.gameObject.GetComponent<VirusMovementBehaviour>();
+		if (virus != null) {
+			print("Loading");
+			SceneManager.LoadScene(linkedScene, LoadSceneMode.Single);
+		}
+
+	}
+}
