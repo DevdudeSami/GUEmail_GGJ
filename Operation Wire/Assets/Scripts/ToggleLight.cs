0c1f3a894994edd02eae82d4dd5cab023050cd11
Youssef Moawad <youssef_programming@icloud.com>
2018-01-28 13:13:04 +0000

    Encryption game added with graphics.

--- Operation Wire/Assets/Scripts/ToggleLight.cs
+++ Operation Wire/Assets/Scripts/ToggleLight.cs
@@ -15,49 +15,17 @@ public class ToggleLight : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 	// Use this for initialization
 	void Awake () {
 		lightController = gameObject.transform.parent.GetComponent<LightSequenceBehaviour>();
-
-		gameObject.GetComponent<Image>().color = Color.white;		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(lightController.shouldAllLightsBeRed) {
-			gameObject.GetComponent<Image>().color = Color.red;
-			isOn = true;
-			return;
-		} else if(lightController.shouldAllLightsBeGreen) {
-			gameObject.GetComponent<Image>().color = Color.green;
-			isOn = true;
-			return;
-		}
-
-		if(lightController.lightShouldBeOn(lightIndex)) {
-			if(!isOn) { turnOn(); }
-		} else {
-			if(isOn) { turnOff(); }
-		}
-	}
-
-	void turnOn() {
-		gameObject.GetComponent<Image>().color = Color.magenta;
-		isOn = true;
-	}
-	void turnOff() {
-		gameObject.GetComponent<Image>().color = Color.white;
-		isOn = false;
 	}
 
 	public void OnPointerDown(PointerEventData eventData) {
 		if(lightController.isWaitingForUserInput) {
-			lightController.lightClicked(lightIndex);
-
-			gameObject.GetComponent<Image>().color = Color.blue;
+			lightController.lightDown(lightIndex);
 		}
 	}
 
 	public void OnPointerUp(PointerEventData eventData) {
 		if(lightController.isWaitingForUserInput) {
-			gameObject.GetComponent<Image>().color = Color.white;
+			lightController.lightUp(lightIndex);
 		}
 	}
 }
