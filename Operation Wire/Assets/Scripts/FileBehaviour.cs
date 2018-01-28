9068856ca36c9de4851a9eb68b6256261f5ec910
Iain Cattermole <iain_c@live.co.uk>
2018-01-28 04:08:23 +0000

    File and PowerUp prefabs

---
+++ Operation Wire/Assets/Scripts/FileBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileBehaviour : MonoBehaviour {
+
+	private BoxCollider2D bc;
+
+	void Awake() {
+		bc = this.gameObject.GetComponent<BoxCollider2D>();
+	}
+
+	void Update() {
+		if (Input.GetKey(KeyCode.Space)) {
+			bc.enabled = true;
+		} else {
+			bc.enabled = false;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		VirusMovementBehaviour virus = coll.gameObject.GetComponent<VirusMovementBehaviour>();
+		if (virus != null) {
+			print("OPEN FILE");
+			// open file
+		}
+	}
+
+}
---
+++ Operation Wire/Assets/Scripts/PowerUpBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PowerUpBehaviour : MonoBehaviour {
+
+	private System.Random rnd;
+
+	void Start() {
+		rnd = new System.Random();
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		VirusMovementBehaviour virus = coll.gameObject.GetComponent<VirusMovementBehaviour>();
+		if (virus != null) {
+
+			VirusMovementBehaviour.PowerUp pup;
+
+			if (rnd.Next(1, 10) == 1) {
+				pup = VirusMovementBehaviour.PowerUp.INVINCIBILITY;
+			} else if (rnd.Next(1, 10) < 7) {
+				pup = VirusMovementBehaviour.PowerUp.SPEED;
+			} else {
+				pup = VirusMovementBehaviour.PowerUp.SLOW;
+			}
+
+			virus.givePowerUp(pup, rnd.Next(200, 400));
+			Destroy(this.gameObject);
+		}
+	}
+
+}
