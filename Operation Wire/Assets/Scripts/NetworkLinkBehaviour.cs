87a5788840a90f6624131796dea0cc46c2557d3d
Iain Cattermole <iain_c@live.co.uk>
2018-01-28 15:31:12 +0000

    target score

--- Operation Wire/Assets/Scripts/NetworkLinkBehaviour.cs
+++ Operation Wire/Assets/Scripts/NetworkLinkBehaviour.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class NetworkLinkBehaviour : MonoBehaviour {
 
 	public string linkedScene;
+	public int targetScore = -1;
+
+
 
 	void OnTriggerEnter2D(Collider2D coll) {
 		VirusMovementBehaviour virus = coll.gameObject.GetComponent<VirusMovementBehaviour>();
-		if (virus != null) {
+		if (virus != null && (targetScore < 0 || virus.totalValue >= targetScore)) {
 			print("Loading");
 			SceneManager.LoadScene(linkedScene, LoadSceneMode.Single);
 		}
