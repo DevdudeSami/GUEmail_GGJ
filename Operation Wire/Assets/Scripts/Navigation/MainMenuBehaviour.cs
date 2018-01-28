39de2408b425f8017db0022b461160b82936da1a
Iain Cattermole <iain_c@live.co.uk>
2018-01-28 14:28:46 +0000

    stuff

---
+++ Operation Wire/Assets/Scripts/Navigation/MainMenuBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuBehaviour : MonoBehaviour {
+
+	private Button btn;
+
+	// Use this for initialization
+	void Awake () {
+		btn = this.gameObject.GetComponent<Button>();
+		print("Loading");
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	// Update is called once per frame
+	void TaskOnClick() {
+		print("Loading");
+		SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+	}
+}
